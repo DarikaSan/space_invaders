01c1d867969f0fee1253b5844d59a7f26bd11ad8
DarikaSan <motto1@live.de>
2021-09-08 15:03:35 +0200

    settings.py angelegt, in der hauptdatei eingepflegt

--- space_invaders.py
+++ space_invaders.py
@@ -1,14 +1,17 @@
 import pygame
 import sys
 
+from settings import Settings
+
 class SpaceInvaders:
     """Overall class to manage game assest and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Invaders")
 
         #Legt die Hintergrundfarbe fest.
@@ -23,7 +26,7 @@ class SpaceInvaders:
                     sys.exit()
             
             #Zeichnet den Bildschirm bei jedem Schleifendurchlauf neu.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Macht den zuletzt gezeichneten Bildschirm sichbar.
             pygame.display.flip()
