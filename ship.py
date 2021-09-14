25060ddfb950943f6b996b92ada56ef5979a1ae9
DarikaSan <motto1@live.de>
2021-09-14 15:07:01 +0200

    ship speed defined

--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ class Ship:
     def __init__(self, si_game):
         """Initialize the ship and set its starting position"""
         self.screen = si_game.screen
+        self.settings = si_game.settings
         self.screen_rect = si_game.screen.get_rect()
 
         # Lädt das Bilddes Schiffes und ruft dessen umgebendes Rechteck ab
@@ -13,16 +14,24 @@ class Ship:
         # Platziert jedes neue Schiff mittig am unteren Bildschirmrand
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Speichert einen Fließkommawert für den Schiffsmittelpunkt
+        self.x = float(self.rect.x)
+
         # Bewegungsflags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flags"""
+        # Aktualisiert den Wert für den Mittelpunkt des Schiffs,
+        # nicht des Rechtecks
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        #Aktualisiert das rect-Objekt auf der Grundlage von self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
