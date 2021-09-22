490d1250e40ee377191401542ad1d385a11dddbb
Marvin Otto <motto1@live.de>
2021-09-22 22:51:40 +0200

    fixing

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = si_game.settings
         self.reset_stats()
 
+        # Startet Space Invaders im aktiven Zustand
+        self.game_active = True
+
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
