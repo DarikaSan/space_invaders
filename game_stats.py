1f0a7721b261bf66e5e16d374be4bfd24d54ee62
Marvin Otto <motto1@live.de>
2021-09-27 11:49:15 +0200

    adding high score

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,8 @@ class GameStats:
 
         # Startet Space Invaders im aktiven Zustand
         self.game_active = False
+        # Der Hightscore darf nie zurückgesetzt werden 
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
